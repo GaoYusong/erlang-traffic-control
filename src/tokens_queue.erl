c14bec1ce2fc31d0d47190d82f8b52fe33797749
GaoYusong <yusong.gao@gmail.com>
2012-11-26 04:46:28 +0800

    complete todo list 3, add two level add_tokens_total for 5 <= cps < 10 and 1 <= cps < 5, if cps is 0, the real cps will be 1, for block forever is not a good method && add TestCase list

--- src/tokens_queue.erl
+++ src/tokens_queue.erl
@@ -17,14 +17,47 @@
 
 %% count_cps_interval must be bigger than add_tokens_interval
 -define(count_cps_interval, 2000).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% begin add_tokens configure 
+
+%% important:
+%% please read the comment and double check before you 
+%% change these values.
+
 %% add_tokens_interval must can be diveded 1000 exactly.
+%% important: 
+%% if you change this value, please change add_tokens_total too,
+%% and the add_tokens_total = 1000 / add_tokens_interval
 -define(add_tokens_interval, 100).
+
 %% this value equals 1000 div add_tokens_interval
+%% important:
+%% if you change this value, please change add_tokens_interval too,
+%% and the add_tokens_interval = 1000 / add_tokens_total
 -define(add_tokens_total, 10).
 
--define(state_tuple, {cps, cps_count, cps_start_time, count_cps_interval,
-	total_tokens, total_tokens_base, total_tokens_reminder, total_tokens_r_count, 
-	count, max_cps, start_time, add_tokens_interval, add_tokens_total}).
+%% same to before, this will be used in max_cps >= 5 && max_cps < 10 
+-define(add_tokens_interval_0, 200).
+-define(add_tokens_total_0, 5).
+
+%% same to before, this will be used in max_cps >= 1 && max_cps < 5
+-define(add_tokens_interval_1, 1000).
+-define(add_tokens_total_1, 1).
+
+%% end add_tokens configure
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+-define(state_tuple, {
+	%% for cps statistics
+	cps, cps_count, cps_start_time, count_cps_interval,
+	%% for tokens limits
+	count, total_tokens,  
+	%% for max_cps set
+	max_cps, total_tokens_base, total_tokens_reminder, total_tokens_r_count,
+	set_add_tokens_interval, set_add_tokens_total,
+	%% current add_tokens interval control
+	start_time, add_tokens_interval, add_tokens_total}).
 
 -record(state, ?state_tuple).
 
@@ -55,24 +88,28 @@ infos(Pid) ->
 request_tokens(Pid) ->
 	gen_server:call(Pid, request_tokens).
 
+%% important: double check every state has a initial value
 init([MAXCPS]) ->
 	State0 = 
 		#state{
+			%% for cps
 			cps 					= 0,
 			cps_count 				= 0,
-			cps_start_time 			= get_now_time(),
-			count_cps_interval 		= ?count_cps_interval,
+			cps_start_time			= get_now_time(),
+			count_cps_interval		= ?count_cps_interval
+
+			%% for count, these will be set in add_tokens
+			% count 					= 0,
 			% total_tokens 			= get_total_tokens(MAXCPS, ?add_tokens_interval),
-			% total_tokens_r_count 	= 0,
-			% total_tokens_reminder	= 0,
-			count 					= 0,
-			max_cps					= MAXCPS,
-			start_time 				= get_now_time(),
-			add_tokens_interval 	= ?add_tokens_interval,
-			add_tokens_total		= ?add_tokens_total
+
+			%% for add_tokens, these will be set in add_tokens
+			% start_time 				= get_now_time(),
+			% add_tokens_interval 	= ?add_tokens_interval,
+			% add_tokens_total		= ?add_tokens_total
 		},
-	State1 = set_total_tokens(State0, MAXCPS, ?add_tokens_total),
-	{ok, State1, 0}.
+	State1 = set_total_tokens(State0, MAXCPS),
+	State2 = add_tokens(State1),
+	{ok, State2, State2#state.add_tokens_interval}.
 
 handle_call(i, _From, State) ->
 	io:format("~p~n", [?state_tuple]),
@@ -82,10 +119,9 @@ handle_call(infos, _From, State) ->
 	Infos = traffic_control_lib:get_infos_from_state(State, ?state_tuple),
 	{reply, Infos, State, get_time_left(State)};
 
-handle_call({set_max_cps, MAXCPS}, _From, State = #state{add_tokens_total = AddTokensTotal}) ->
-	State0 = State#state{max_cps = MAXCPS},
-	State1 = set_total_tokens(State0, MAXCPS, AddTokensTotal),
-	{reply, ok, State1, get_time_left(State)};
+handle_call({set_max_cps, MAXCPS}, _From, State) ->
+	State0 = set_total_tokens(State, MAXCPS),
+	{reply, ok, State0, get_time_left(State)};
 
 % handle_call({set_cps_interval, CountCpsInterval}, _From, State) ->
 % 	{reply, ok, State#state{count_cps_interval = CountCpsInterval}, get_time_left(State)};
@@ -105,10 +141,9 @@ handle_call(request_tokens, _From, State = #state{cps_count = CpsCount, count = 
 				State#state.add_tokens_interval}
 	end.
 
-handle_cast({set_max_cps_cast, MAXCPS}, State = #state{add_tokens_total = AddTokensTotal}) ->
-	State0 = State#state{max_cps = MAXCPS},
-	State1 = set_total_tokens(State0, MAXCPS, AddTokensTotal),
-	{noreply, State1, get_time_left(State)};
+handle_cast({set_max_cps_cast, MAXCPS}, State) ->
+	State0 = set_total_tokens(State, MAXCPS),
+	{noreply, State0, get_time_left(State)};
 
 handle_cast(_Event, State) ->
     {noreply, State}.
@@ -143,7 +178,10 @@ time_left(StartTime, Interval) ->
 % 	MaxCps * Interval div 1000.
 
 add_tokens(State 
-		% = #state{cps_count = CpsCount, cps_start_time = CpsStartTime, count_cps_interval = CountCpsInterval}
+		= #state{
+			cps_count = CpsCount, cps_start_time = CpsStartTime, count_cps_interval = CountCpsInterval,
+			set_add_tokens_interval = SetAddTokensInterval, set_add_tokens_total = SetAddTokensTotal
+		}
 	) ->
 
 	% io:format("Infos ~p NowTime ~p~n", [traffic_control_lib:get_infos_from_state(State, ?state_tuple), get_now_time()]),
@@ -151,32 +189,53 @@ add_tokens(State
 	NowTime = get_now_time(),
 
 	State0 = State#state{
-		count = 0, start_time = NowTime
+		count = 0, 
+		start_time = NowTime, add_tokens_interval = SetAddTokensInterval, add_tokens_total = SetAddTokensTotal
 	},
 
-	% Interval = NowTime - CpsStartTime,
-	% State1 = case  Interval >= CountCpsInterval of
-	% 	true ->
-	% 		NowCps = CpsCount / Interval * 1000,
-	% 		% io:format("Current cps is ~p~n", [NowCps]),
-	% 		State0#state{cps = NowCps, cps_count = 0, cps_start_time = NowTime};
-	% 	false ->
-	% 		State0
-	% end,
-	State1 = State0,
+	Interval = NowTime - CpsStartTime,
+	State1 = case  Interval >= CountCpsInterval of
+		true ->
+			NowCps = CpsCount / Interval * 1000,
+			% io:format("Current cps is ~p~n", [NowCps]),
+			State0#state{cps = NowCps, cps_count = 0, cps_start_time = NowTime};
+		false ->
+			State0
+	end,
+	% State1 = State0,
 
+	%% add_tokens_total must be set before call calc_total_tokens
 	State2 = calc_total_tokens(State1),
 	State2.
 
-set_total_tokens(State, MaxCps, AddTokensTotal) ->
-	TotalTokens = MaxCps div AddTokensTotal,
-	Reminder 	= MaxCps rem AddTokensTotal, 
+set_total_tokens(State, MaxCps) ->
+	{SetAddTokensInterval, SetAddTokensTotal}
+		= get_set_add_tokens_info(MaxCps),
+
+	TotalTokens = MaxCps div SetAddTokensTotal,
+	Reminder 	= MaxCps rem SetAddTokensTotal, 
 	State0 = State#state{
+		max_cps 				= MaxCps,
 		total_tokens_base		= TotalTokens,
 		total_tokens_reminder 	= Reminder,
-		total_tokens_r_count 	= 0
+		total_tokens_r_count 	= 0,
+		set_add_tokens_interval = SetAddTokensInterval,
+		set_add_tokens_total 	= SetAddTokensTotal
 	},
 	State0.
+
+get_set_add_tokens_info(MaxCps) ->
+	case MaxCps >= ?add_tokens_total of
+		true ->
+			{?add_tokens_interval, ?add_tokens_total};
+		false ->
+			case MaxCps >= ?add_tokens_total_0 of
+				true ->
+					{?add_tokens_interval_0, ?add_tokens_total_0};
+				false ->
+					{?add_tokens_interval_1, ?add_tokens_total_1}
+			end
+	end.
 
 calc_total_tokens(State = #state{
 		total_tokens_base		= TotalTokensBase,
@@ -199,3 +258,4 @@ calc_total_tokens(State = #state{
 				TotalTokensBase
 		end,
 	State#state{total_tokens = TotalTokens, total_tokens_r_count = RCount0 + 1}.
+
