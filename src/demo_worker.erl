ecf047aba960d483996e0a548952a5a14f03b61a
GaoYusong <yusong.gao@gmail.com>
2013-03-07 23:54:12 +0800

    update callback_function method demo program, make it more clarity

--- src/demo_worker.erl
+++ src/demo_worker.erl
@@ -2,28 +2,13 @@
 
 -compile(export_all).
 
+
 start(Pid) ->
-	start(10000, Pid).
+	start(1000000, Pid).
 
 start(N, Pid) ->
 	spawn(fun() ->
 		for(N, fun() -> tokens_queue:request_tokens(Pid) end)
-	end).
-
-start_with_callback(Pid) ->
-	start_with_callback(10000, Pid).
-
-start_with_callback(N, Pid) ->
-	spawn(fun() ->
-		Self = self(),
-		tokens_queue:set_callback_function(Pid, fun() -> Self ! wakeup end),
-		for(N, fun() ->
-			tokens_queue:request_tokens_cast(Pid),
-			receive
-				wakeup ->
-					ok
-			end
-		end)
 	end).
 
 for (0, _Func) ->
@@ -31,3 +16,22 @@ for (0, _Func) ->
 for (N, Func) ->
 	Func(),
 	for (N - 1, Func).
+
+start_with_callback(Pid) ->
+	spawn(fun() ->
+		Self = self(),
+		tokens_queue:set_callback_function(Pid, fun() -> Self ! wakeup end),
+		tokens_queue:request_tokens_cast(Pid),
+		loop(Pid)
+	end).
+
+loop(Pid) ->
+	receive
+		wakeup ->
+			tokens_queue:request_tokens_cast(Pid),
+			loop(Pid);
+		_Other ->
+			io:format("I am fine!~n"),
+			loop(Pid)
+	end.
+
